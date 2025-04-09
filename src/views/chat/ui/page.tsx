ba59410c371f99d2cc05560e3035762b2e080ac8
nijesmik <bkybb@duck.com>
2025-04-10 05:04:01 +0900

    :sparkles: 음성 채팅 컨트롤 UI 추가

---
+++ src/views/chat/ui/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { AudioLines, Mic, MicOff, X } from "lucide-react";
+import { useCallback, useState } from "react";
+
+import { useVoiceChatConnect, useVoiceChatStore } from "@/features/voice-chat";
+import { Button, type ButtonProps } from "@/shared/components";
+
+const ButtonToggleMute = () => {
+  const { isMuted, toggleMute } = useVoiceChatStore();
+  const buttonVariant: ButtonProps["variant"] = isMuted
+    ? "destructive"
+    : "secondary";
+
+  return (
+    <Button onClick={toggleMute} variant={buttonVariant} size="icon">
+      {isMuted ? <MicOff /> : <Mic />}
+    </Button>
+  );
+};
+
+const ButtonStartConversation = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { isConnected, connect, disconnect } = useVoiceChatConnect();
+
+  const handleClick = useCallback(() => {
+    if (!isConnected) {
+      setIsLoading(true);
+      connect();
+    } else {
+      setIsLoading(false);
+      disconnect();
+    }
+  }, [isConnected]);
+
+  if (!isConnected) {
+    return (
+      <Button onClick={handleClick} isLoading={isLoading} size="icon">
+        <AudioLines />
+      </Button>
+    );
+  }
+
+  return (
+    <div className="flex gap-2">
+      <ButtonToggleMute />
+      <Button onClick={handleClick} size="icon">
+        <X />
+      </Button>
+    </div>
+  );
+};
+
+export default function Home() {
+  return (
+    <main className="h-screen w-screen">
+      <div className="flex h-full w-full items-center justify-center">
+        <ButtonStartConversation />
+      </div>
+    </main>
+  );
+}
