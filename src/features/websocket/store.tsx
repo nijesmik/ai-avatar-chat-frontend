28538c1a61df95f778f029dd171913397f9bb059
nijesmik <bkybb@duck.com>
2025-04-18 06:27:51 +0900

    :sparkles: 웹소켓 재연결 시도 설정 및 연결 실패 토스트 추가

--- src/features/websocket/store.tsx
+++ src/features/websocket/store.tsx
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
+import { toast } from "sonner";
 import { create } from "zustand";
 
 import { WEBSOCKET_BASE_URL } from "@/shared/config";
@@ -7,6 +8,18 @@ interface WebSocketStore {
   socket: Socket;
 }
 
-export const useWebSocketStore = create<WebSocketStore>(() => ({
-  socket: io(WEBSOCKET_BASE_URL),
-}));
+export const useWebSocketStore = create<WebSocketStore>(() => {
+  const socket = io(WEBSOCKET_BASE_URL, {
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 2000,
+  });
+
+  socket.io.on("reconnect_failed", () => {
+    toast.error("서버 연결에 실패했습니다. 나중에 다시 시도해 주세요.");
+  });
+
+  return {
+    socket,
+  };
+});
