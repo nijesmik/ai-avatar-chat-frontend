0a6f608c2087697f4a64f4617279c4a8b0dbc55c
nijesmik <bkybb@duck.com>
2025-04-26 02:51:39 +0900

    :lipstick: 글로벌 스타일 수정

--- src/app/styles/heroui.ts
+++ src/app/styles/heroui.ts
@@ -1,3 +1,26 @@
 import { heroui } from "@heroui/react";
 
-export default heroui();
+export default heroui({
+  themes: {
+    light: {
+      colors: {
+        primary: {
+          DEFAULT: "#000000",
+        },
+        danger: {
+          50: "#fef1f1",
+          100: "#fdd8db",
+          200: "#fbb7bb",
+          300: "#f9959c",
+          400: "#f7737c",
+          500: "#f5535e",
+          600: "#ec323e",
+          700: "#d91c29",
+          800: "#ae1e27",
+          900: "#8f1e26",
+          DEFAULT: "#f5535e",
+        },
+      },
+    },
+  },
+});
