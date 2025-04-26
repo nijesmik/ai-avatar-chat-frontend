b2bc0d8156d7bf9072bbd8dadddf2d1d3f470ace
nijesmik <bkybb@duck.com>
2025-04-27 04:43:12 +0900

    :lipstick: toast offet 설정 추가

--- src/app/providers/toast.tsx
+++ src/app/providers/toast.tsx
@@ -3,7 +3,7 @@
 import { ToastProvider as HeroUIToastProvider } from "@heroui/react";
 
 const ToastProvider = () => {
-  return <HeroUIToastProvider placement="top-center" />;
+  return <HeroUIToastProvider placement="top-center" toastOffset={24} />;
 };
 
 export default ToastProvider;
